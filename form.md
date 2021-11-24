0c5e4116f2c8845b2826e8e896898e5a25870eb8
GitJournal <app@gitjournal.io>
2021-11-24 16:53:27 +0300

    Edited Note

--- form.md
+++ form.md
@@ -1,35 +1,5 @@
-<html>
-<head>
-<title>Форма заявки с сайта</title>
-</head>
-<body>
-<?php
-//проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['fio']) and !isset($_POST['email'])){
- ?> <form action="send.php" method="post">
+<form action="send.php" method="post">
 <input type="text" name="fio" placeholder="Укажите ФИО" required>
 <input type="text" name="email" placeholder="Укажите e-mail" required>
 <input type="submit" value="Отправить">
-</form> 
-
-<?php
-} else {показываем форму
- $fio = $_POST['fio'];
- $email = $_POST['email'];
- $fio = htmlspecialchars($fio);
- $email = htmlspecialchars($email);
- $fio = urldecode($fio);
- $email = urldecode($email);
- $fio = trim($fio);
- $email = trim($email);
- if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
- echo "Сообщение успешно отправлено";
- } else {
- echo "При отправке сообщения возникли ошибки";
- }
-}
-?>
-</body>
-</html>
-
-[send](send.php)
+</form>
--- send.php
+++ send.php
@@ -1,32 +1,17 @@
-<html>
-<head>
-<title>Форма заявки с сайта</title>
-</head>
-<body>
 <?php
-//проверяем, существуют ли переменные в массиве POST
-if(!isset($_POST['fio']) and !isset($_POST['email'])){
- ?> <form action="send.php" method="post">
-<input type="text" name="fio" placeholder="Укажите ФИО" required>
-<input type="text" name="email" placeholder="Укажите e-mail" required>
-<input type="submit" value="Отправить">
-</form> <?php
+$fio = $_POST['fio'];
+$email = $_POST['email'];
+$fio = htmlspecialchars($fio);
+$email = htmlspecialchars($email);
+$fio = urldecode($fio);
+$email = urldecode($email);
+$fio = trim($fio);
+$email = trim($email);
+//echo $fio;
+//echo "<br>";
+//echo $email;
+if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+ {     echo "сообщение успешно отправлено";
 } else {
-показываем форму
- $fio = $_POST['fio'];
- $email = $_POST['email'];
- $fio = htmlspecialchars($fio);
- $email = htmlspecialchars($email);
- $fio = urldecode($fio);
- $email = urldecode($email);
- $fio = trim($fio);
- $email = trim($email);
- if (mail("talalakin@rambler.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n")){
- echo "Сообщение успешно отправлено";
- } else {
- echo "При отправке сообщения возникли ошибки";
- }
-}
-?>
-</body>
-</html>
+    echo "при отправке сообщения возникли ошибки";
+}?>
