38452c00d7f93108f8986fbf67e0933d0df1f515
GitJournal <app@gitjournal.io>
2021-11-24 17:18:43 +0300

    Edited Note

---
+++ form.php
@@ -0,0 +1,5 @@
+<form action="https://talalakinai.github.io/OSB/send.php="post">
+<input type="text" name="fio" placeholder="Укажите ФИО" required>
+<input type="text" name="email" placeholder="Укажите e-mail" required>
+<input type="submit" value="Отправить">
+</form>
--- send.php
+++ send.php
@@ -7,10 +7,10 @@ $fio = urldecode($fio);
 $email = urldecode($email);
 $fio = trim($fio);
 $email = trim($email);
-//echo $fio;
-//echo "<br>";
-//echo $email;
-if (mail("example@mail.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
+echo $fio;
+echo "<br>";
+echo $email;
+if (mail("talalakin@rambler.ru", "Заявка с сайта", "ФИО:".$fio.". E-mail: ".$email ,"From: example2@mail.ru \r\n"))
  {     echo "сообщение успешно отправлено";
 } else {
     echo "при отправке сообщения возникли ошибки";
